fceb410abe5539ed16a42f7add5ac047b8a10450
vidanmai <vidanmai@gmail.com>
2020-03-08 22:58:11 +0100

    Added Materialize UI

--- js/main.js
+++ js/main.js
@@ -6,3 +6,31 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+const sideNav = document.querySelectorAll('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+const ss = document.querySelectorAll('.scrollspy');
+M.ScrollSpy.init(ss, {});
+
+const dropDown = document.querySelectorAll('.dropdown-trigger');
+M.Dropdown.init(dropDown, {});
+
+const collapsible = document.querySelectorAll('.collapsible');
+M.Collapsible.init(collapsible, {
+    accordion: false
+});
+
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax, {});
+
+const tabs = document.querySelectorAll('.tabs');
+M.Tabs.init(tabs, {
+    swipeable: true
+});
+
+
+const fab = document.querySelector('.fixed-action-btn');
+M.FloatingActionButton.init(fab, {
+    direction: 'left'
+});
